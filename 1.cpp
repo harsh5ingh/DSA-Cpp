614641c91aa14c549d975ba85abf6efb1c33b849
Harsh <harrsh0502@gmail.com>
2025-10-04 21:53:50 +0530

    update

--- 1.cpp
+++ 1.cpp
@@ -1,12 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main() {
-  //int
+int main()
+{
+  // int
   int x = 10;
   cin >> x;
   cout << "The value of x is: " << x << endl;
-  //long long
+  // long long
   long long y = 10000000000;
   cin >> y;
   cout << "The value of y is: " << y << endl;
---
+++ patten1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+class Solution {
+public:
+    void pattern1(int n) {
+        for(int i=1; i<=n; i++){
+         for(int j=1; j<=n; j++){
+         cout<< "*";
+    }
+    cout<<endl;
+        }
+        
+    }
+    
+};
