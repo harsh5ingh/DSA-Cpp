249a77733a949c0eb6cedcce19a567aabd7d8598
Harsh <95859326+harsh5ingh@users.noreply.github.com>
2025-10-13 14:00:16 +0000

    updated loop

---
+++ loop1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+  int count;
+    cout << "Enter the number:  \n";
+      cin >> count;
+
+  while (count <= 20)
+  {
+    cout<<count<<endl;
+     count++;
+    //count += 1;
+    // count = count + 1;
+  }
+  
+
+
+}
