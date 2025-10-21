7679fbc3cf42891a0f36ebc5c9f9305481ec2ec7
Harsh <harrsh0502@gmail.com>
2025-10-22 03:53:08 +0530

    pattern 3

---
+++ pattern3.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int n = 3;
+ // int sum;
+  //cin >> n;
+
+  for (int i = 1; i <= n; i++) // outer loop
+  {
+
+    for (int j = 1; j <=i; j++) // inner loop
+    {
+      cout << j;
+      //j+=1;
+    }
+    cout << endl;
+  }
+  return 0;
+}
