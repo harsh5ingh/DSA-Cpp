dca1b2df1b69c77636b2df515d87363a057bce06
Harsh <harrsh0502@gmail.com>
2025-11-12 22:49:12 +0530

    update

--- function1.cpp
+++ function1.cpp
@@ -9,9 +9,18 @@ void printHello()
   cout << "hello\n";
 }
 
+// sum of 2 numbers
+
+double sum(double a, double b) {
+  double s = a + b;
+  return s;
+}
+
 int main()
 {
   // Function Call/Invoke
   printHello();
+
+  cout<<sum(5.56,6.23)<<endl;
   return 0;
 }
