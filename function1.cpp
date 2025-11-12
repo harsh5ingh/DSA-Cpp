c21d716ec0e6c0f22995255e664a1f99b5c21a99
Harsh <harrsh0502@gmail.com>
2025-11-12 20:57:53 +0530

    function

---
+++ function1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+
+// Function Defination
+
+void printHello()
+{
+  cout << "hello\n";
+}
+
+int main()
+{
+  // Function Call/Invoke
+  printHello();
+  return 0;
+}
