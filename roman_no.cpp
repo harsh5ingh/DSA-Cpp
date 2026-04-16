8690bdbf4c6803a742d33cc5b0f44ab78da12237
Harsh <harrsh0502@gmail.com>
2026-04-17 00:43:22 +0530

    revision

---
+++ roman_no.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int I = 1;
+  int X = 10;
+  int L = 50;
+  int C = 100;
+  int D = 500;
+  int M = 1000;
+  int n = 0;
+  if (n == 0){ 
+    cout << "Enter a number: ";
+    cin >> n;
+    
+  }
+  
+}
