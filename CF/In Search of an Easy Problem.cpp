d0a616fa24dfafd17903e7bd5d460a2695ceb5d4
Harsh <harrsh0502@gmail.com>
2026-04-23 00:18:02 +0530

    CF9

---
+++ CF/In Search of an Easy Problem.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int n;
+  cin >> n;
+  for (int m = 0; m <= n; m++)
+  {
+    int x;
+    cin >> x;
+    if (x == 1)
+    {
+      }
+    else
+    {
+      cout << "Easy";
+    }
+  }
+  cout << "Hard";
+}
