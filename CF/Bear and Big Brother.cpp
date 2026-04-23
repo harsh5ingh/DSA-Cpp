886c3e5b27950a727333494d0526925c9eaa1607
Harsh <harrsh0502@gmail.com>
2026-04-23 23:40:59 +0530

    CF_10

--- CF/Bear and Big Brother.cpp
+++ CF/Bear and Big Brother.cpp
@@ -1,17 +1,18 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
-  int a,b;
+int main()
+{
+  int a, b;
   cin >> a >> b;
   int year = 0;
-  for (int i = 1; a <= b; i++)
+  // for (int i = 1; a <= b; i++) // better for loop
+  while (a <= b) // better
   {
-    a *= 3; 
-    b *= 2 ;  
-    year++; 
+    a *= 3;
+    b *= 2;
+    year++;
   }
-  cout << year; 
+  cout << year;
   return 0;
-  
 }
