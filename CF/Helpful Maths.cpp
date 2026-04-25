d641d7c5751ae0f1be0cd1bd2bd1190d999a9187
Harsh <harrsh0502@gmail.com>
2026-04-25 23:51:26 +0530

    CF_11

---
+++ CF/Helpful Maths.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+
+    sort(s.begin(), s.end());
+
+    for (int i = 0; i < s.length(); i++) {
+        if (s[i] != '+') {
+            cout << s[i];
+
+            // check if any number still remains ahead
+            for (int j = i + 1; j < s.length(); j++) {
+                if (s[j] != '+') {
+                    cout << "+";
+                    break;
+                }
+            }
+        }
+    }
+
+    return 0;
+}
